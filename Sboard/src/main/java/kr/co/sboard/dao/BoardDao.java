2fd0fdd2483a4dee67ccb54c9074172a822237ee
traver9044@gmail.com <traver9044@gmail.com>
2021-02-23 12:17:33 +0900

    2021/02/23 게시판 만들기

--- Sboard/src/main/java/kr/co/sboard/dao/BoardDao.java
+++ Sboard/src/main/java/kr/co/sboard/dao/BoardDao.java
@@ -1,5 +1,20 @@
 package kr.co.sboard.dao;
 
-public class BoardDao {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import kr.co.sboard.vo.BoardVo;
+
+@Repository
+public interface BoardDao {
+	
+	public void insertArticle(BoardVo vo);
+	
+	public int selectCountTotalArticle();
+	public BoardVo selectArticle(int seq);
+	public List<BoardVo> selectArticles(int start);
+	public void updateArticle(BoardVo vo);
+	public void deleteArticle();
+	
 }
