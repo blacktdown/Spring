7eb67823d82dc641be06b07e9a8cd55df658aed0
traver9044@gmail.com <traver9044@gmail.com>
2021-03-03 17:03:10 +0900

    2021/03/03 Kmarket Admin 등록 및 설정

--- Kmarket/src/main/java/kr/co/kmarket/dao/AdminDao.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/AdminDao.java
@@ -1,5 +1,19 @@
 package kr.co.kmarket.dao;
 
-public interface AdminDao {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import kr.co.kmarket.vo.Cate1Vo;
+import kr.co.kmarket.vo.Cate2Vo;
+import kr.co.kmarket.vo.ProductsVo;
+
+@Repository
+public interface AdminDao {
+	
+	public List<Cate1Vo> selectCate1();
+	public List<Cate2Vo> selectCate2(int code1);
+	
+	public void insertProduct(ProductsVo vo);
+	
 }
