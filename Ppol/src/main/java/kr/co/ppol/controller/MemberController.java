a7f78768b575cad8af879de84b3e195cc9ab5428
traver9044@gmail.com <traver9044@gmail.com>
2021-03-08 15:35:39 +0900

    포트폴리오 작성중

---
+++ Ppol/src/main/java/kr/co/ppol/controller/MemberController.java
@@ -0,0 +1,39 @@
+package kr.co.ppol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	
+	@GetMapping("/member/join")
+	public String join() {
+		return "/member/join";
+	}
+	
+	@GetMapping("/member/login")
+	public String login() {
+		return "/member/login";
+	}
+	
+	@GetMapping("/member/terms")
+	public String terms() {
+		return "/member/terms";
+	}
+	
+	@GetMapping("/member/register")
+	public String register() {
+		return "/member/register";
+	}
+	
+	@GetMapping("/member/register-seller")
+	public String registerSeller() {
+		return "/member/register-seller";
+	}
+	
+	@GetMapping("/member/signup")
+	public String signup() {
+		return "/member/signup";
+	}
+
+}
